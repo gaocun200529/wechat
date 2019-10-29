f5d92f4000ebf761304219d15b93e733345db837
Gao200529 <2501904726@qq.con>
2019-10-29 20:23:42 +0800

    first commit

--- app/Http/Controllers/Index/EventController.php
+++ app/Http/Controllers/Index/EventController.php
@@ -20,11 +20,12 @@ class EventController extends Controller
     }
 
     public function event(){
-       $info = file_get_contents("php://input");
-        file_put_contents(storage_path('logs/wexin/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
-        file_put_contents(storage_path('logs/wexin/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
-        $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
-        $xml_arr = (array)$xml_obj;
+      echo  $_GET['echostr'];
+       // $info = file_get_contents("php://input");
+       //  file_put_contents(storage_path('logs/wexin/'.date('Y-m-d').'.log'),"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+       //  file_put_contents(storage_path('logs/wexin/'.date('Y-m-d').'.log'),$info,FILE_APPEND);
+       //  $xml_obj = simplexml_load_string($info,'SimpleXMLElement',LIBXML_NOCDATA);
+       //  $xml_arr = (array)$xml_obj;
         // 关注操作
   //     if($xml_arr['MsgType']=='event' && $xml_arr['Event']=='subscribe'){
   //            //判断openid表是否有当前openid
